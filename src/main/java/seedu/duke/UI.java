c14d8c04f4760947972c3543c9c62aaa2a837d4d
Chenling CUI <cuichenling2@gmail.com>
2021-03-11 11:03:36 +0800

    Modified coding style

--- src/main/java/seedu/duke/UI.java
+++ src/main/java/seedu/duke/UI.java
@@ -2,13 +2,14 @@ package seedu.duke;
 import java.util.Scanner;
 public class UI {
     public static Scanner in = new Scanner(System.in);
+
     public UI() {
     }
-    protected static String getString(Scanner in) {
-        String user_input;
-        user_input = in.nextLine();
-        return user_input;
 
+    protected static String getString(Scanner in) {
+        String userInput;
+        userInput = in.nextLine();
+        return userInput;
     }
 
 
