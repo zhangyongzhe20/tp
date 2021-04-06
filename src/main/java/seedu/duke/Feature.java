0d3276c44fa935115404aa2cd29c4b3601f07c50
cswbibibi <caoshuwen1017@gmail.com>
2021-04-06 20:50:20 +0800

    update code to make listAllLocation more case insentive

--- src/main/java/seedu/duke/Feature.java
+++ src/main/java/seedu/duke/Feature.java
@@ -8,40 +8,34 @@ import java.util.List;
 
 //@@cswbibibi
 public class Feature {
-    public static <T> List<String> listAllLocations(String location) {
-        List<String> list = new ArrayList<>();
-        switch (location) {
-        case "Canteen":
-        case "canteen":
-        case "CANTEEN":
-            loadData ld = new loadData();
-            List<Facility> canteens = ld.getCanteens();
-            for (int i=0; i<canteens.size(); i++) {
-                System.out.println(canteens.get(i).getName() + "@" + canteens.get(i).getAddress());
-                list.add(canteens.get(i).getName() + "@" + canteens.get(i).getAddress());
+    public static <T> List<String> listAllLocations(String location) throws NullPointerException {
+        List<String> list = null;
+        try {
+            list = new ArrayList<>();
+            if (location.equalsIgnoreCase("canteen")) {
+                loadData ld = new loadData();
+                List<Facility> canteens = ld.getCanteens();
+                for (int i = 0; i < canteens.size(); i++) {
+                    System.out.println(canteens.get(i).getName() + "@" + canteens.get(i).getAddress());
+                    list.add(canteens.get(i).getName() + "@" + canteens.get(i).getAddress());
+                }
+            } else if (location.equalsIgnoreCase("lecture theater")) {
+                List<Facility> lectureTheaters = loadData.getLectureTheaters();
+                for (int i = 0; i < lectureTheaters.size(); i++) {
+                    System.out.println(lectureTheaters.get(i).getName() + "@" + lectureTheaters.get(i).getAddress());
+                    list.add(lectureTheaters.get(i).getName() + "@" + lectureTheaters.get(i).getAddress());
+                }
+            } else if (location.equalsIgnoreCase("library")) {
+                List<Facility> libraries = loadData.getLibraries();
+                for (int i = 0; i < libraries.size(); i++) {
+                    System.out.println(libraries.get(i).getName() + "@" + libraries.get(i).getAddress());
+                    list.add(libraries.get(i).getName() + "@" + libraries.get(i).getAddress());
+                }
+            } else {
+                System.out.println("Sorry, there's no such facility");
             }
-            break;
-        case "Lecture Theater":
-        case "lecture theater":
-        case "Lecture theater":
-        case "LECTURE THEATER":
-            List<Facility> lectureTheaters = loadData.getLectureTheaters();
-            for (int i=0; i<lectureTheaters.size(); i++) {
-                System.out.println(lectureTheaters.get(i).getName() + "@" + lectureTheaters.get(i).getAddress());
-                list.add(lectureTheaters.get(i).getName() + "@" + lectureTheaters.get(i).getAddress());
-            }
-            break;
-        case "Library":
-        case "LIBRARY":
-        case "library":
-            List<Facility> libraries = loadData.getLibraries();
-            for (int i=0; i<libraries.size(); i++) {
-                System.out.println(libraries.get(i).getName() + "@" + libraries.get(i).getAddress());
-                list.add(libraries.get(i).getName() + "@" + libraries.get(i).getAddress());
-            }
-            break;
-        default:
-            break;
+        } catch (Exception e) {
+            System.out.println("Sorry, there's no such facility");
         }
         return list;
     }
